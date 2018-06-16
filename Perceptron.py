8a3e6bf8d5f16ce1d2b0f745f5d8648e94458eeb
Rjack96 <rgiachin@gmail.com>
2018-06-16 10:29:06 +0200

    Add files via upload
    a possible complete implementation of perceptron.

--- Perceptron.py
+++ Perceptron.py
@@ -5,10 +5,26 @@ class Perceptron:
     def __init__(self, eta, epochs):
         eta = 0.25
         epochs = 100
+        self.w = np.zeros(len(X[0]))  # initializing a vector representing the weights
     def training(self, X, Y):
-        self.w = np.zeros(len(X[0])) #initializing a vector representing the weights
         k = 0
         i = 0
+        vec = self.norm(X)
+        while True:
+            nErr = 0
+            for x, y in zip(X, Y):
+                if y*self.future(x)<0:
+                    self.w[1:] = [fx + self.eta*(fy*y) for fx, fy in zip(self.w[1:], x)] #it allows to read inside a list.
+                    self.w[0] += self.eta *y*math.pow(vec,2)
+                    k += 1
+                    nErr = nErr + 1
+            i = i+1
+            print "number of errors:", nErr
+            if nErr == 0 or i > self.epochs:
+                break
+        if i > self.epochs:
+            print "an error has occured"
+            return self.w, i-1, k
 
 
 
@@ -25,4 +41,4 @@ class Perceptron:
 
     def future(self, x):
         return 1 if self.input(x)>= 0.0 else 0
-    
+
