d96c07c0ef4db8ab8782dbc0e49902bd4fbb1026
Rjack96 <rgiachin@gmail.com>
2018-06-19 11:47:13 +0200

    Add files via upload
    finished perceptron

--- Perceptron.py
+++ Perceptron.py
@@ -2,20 +2,20 @@ import numpy as np
 import math
 
 class Perceptron:
-    def __init__(self, eta, epochs):
-        eta = 0.25
-        epochs = 100
-        self.w = np.zeros(len(X[0]))  # initializing a vector representing the weights
+    def __init__(self, bias, epochs):
+        self.bias = bias
+        self.epochs = epochs
     def training(self, X, Y):
         k = 0
         i = 0
+        self.w = np.zeros(1+X.shape[1])  # initializing a vector representing the weights
         vec = self.norm(X)
         while True:
             nErr = 0
             for x, y in zip(X, Y):
-                if y*self.future(x)<0:
-                    self.w[1:] = [fx + self.eta*(fy*y) for fx, fy in zip(self.w[1:], x)] #it allows to read inside a list.
-                    self.w[0] += self.eta *y*math.pow(vec,2)
+                if y*self.guess(x) < 0:
+                    self.w[1:] = [fx + self.bias * (fy * y) for fx, fy in zip(self.w[1:], x)] #it allows to read inside a list.
+                    self.w[0] += self.bias * y * math.pow(vec, 2)
                     k += 1
                     nErr = nErr + 1
             i = i+1
@@ -39,6 +39,6 @@ class Perceptron:
     def input(self,x):
         return np.dot(x, self.w) #it makes the multiplication between an element of the input vector and the initial weight
 
-    def future(self, x):
-        return 1 if self.input(x)>= 0.0 else 0
+    def guess(self, x):
+        return np.where(self.net_input(x) >= 0.0, 1, -1)
 
